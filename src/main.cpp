304db60ca43cfa4b27308d69ebc91fa92d7c85ac
Mlessqu <53700875+Mlessqu@users.noreply.github.com>
2024-12-25 20:49:03 +0100

    yes

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,23 @@
 
 int main()
 {
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
+    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "Playfield SFML3.0");
     window.setFramerateLimit(144);
 
     while (window.isOpen())
     {
-        while (const std::optional event = window.pollEvent())
+        while (const std::optional _event = window.pollEvent())
         {
-            if (event->is<sf::Event::Closed>())
+            if (_event->is<sf::Event::Closed>())
+            {
+                window.close();
+            }else if(const auto* key_pressed = _event->getIf<sf::Event::KeyPressed>())
+        {
+            if( key_pressed->scancode == sf::Keyboard::Scancode::Escape)
             {
                 window.close();
             }
+        }
         }
 
         window.clear();
