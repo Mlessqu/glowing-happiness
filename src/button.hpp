fbc18bc17137ccbb66633a101231f6d34a597e73
Mlessqu <53700875+Mlessqu@users.noreply.github.com>
2025-01-10 07:12:44 +0100

    sdas

--- src/button.hpp
+++ src/button.hpp
@@ -9,8 +9,8 @@ public:
     Button(sf::Vector2f _size, sf::Vector2f _pos, std::string _caption);
 
 private:
-    // sf::Font font;
-    // sf::Text text;
+    sf::Font font;
+    sf::Text text;
 
     void draw(sf::RenderTarget &target, sf::RenderStates states) const override
     {
@@ -19,7 +19,7 @@ private:
     }
 };
 
-Button::Button(sf::Vector2f _size, sf::Vector2f _pos, std::string _caption)
+Button::Button(sf::Vector2f _size, sf::Vector2f _pos, std::string _caption) : font("arial.ttf"), text(font)
 {
     // font.openFromFile("arial.ttf");
 
@@ -28,12 +28,11 @@ Button::Button(sf::Vector2f _size, sf::Vector2f _pos, std::string _caption)
     button_body.setFillColor(sf::Color::Green);
     button_body.setOrigin({_size.x / 2, _size.y / 2});
     button_body.setPosition(_pos);
-    /*-------
+    //-------
     text.setFont(font);
     text.setFillColor(sf::Color::Blue);
     text.setStyle(sf::Text::Bold);
     text.setPosition(button_body.getPosition());
     text.setString(_caption);
     text.setCharacterSize(24);
-    */
 }
